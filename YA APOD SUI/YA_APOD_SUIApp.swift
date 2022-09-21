b4ce23c9ba381427a886a56ef945068561d3a62c
Andrew <AndSwiftUs@gmail.com>
2022-09-21 11:43:07 +0300

    Initial Commit

---
+++ YA APOD SUI/YA_APOD_SUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  YA_APOD_SUIApp.swift
+//  YA APOD SUI
+//
+//  Created by Andrew Us on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct YA_APOD_SUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
