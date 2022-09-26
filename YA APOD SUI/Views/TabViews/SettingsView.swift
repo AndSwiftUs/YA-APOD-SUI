7468694fcf681dd5a02cb5f50d6a58b4a784bf47
Andrew Ustintchik <AndSwiftUs@gmail.com>
2022-09-26 21:19:40 +0300

    small fix for future localizaton

--- YA APOD SUI/Views/TabViews/SettingsView.swift
+++ YA APOD SUI/Views/TabViews/SettingsView.swift
@@ -32,8 +32,14 @@ struct SettingsView: View {
                     
                     // Stepper here for debug -> change to Text
                     // planing to get remainingRequest by inspecting the X-RateLimit-Limit and X-RateLimit-Remaining HTTP headers
-                    Stepper("Request today: \(remainingRequest)", value: $remainingRequest)
-                    
+                    Stepper(value: $remainingRequest) {
+                        HStack {
+                            Text("Request today: ")
+                            Spacer()
+                            Text("\(remainingRequest)")
+                        }
+                    }
+                        
                     if userAPIKey == "DEMO_KEY" {
                         
                         Text("\"DEMO_KEY\" is default API-key that have some restrictions:")
